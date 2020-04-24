c29eb427238239c30416f3b88bea31da0b9fc553
billiesu <54905672+billiesu@users.noreply.github.com>
2020-04-24 16:20:03 -0700

    commit

--- backend/frontend/public/index.js
+++ backend/frontend/public/index.js
@@ -13,11 +13,11 @@ async function submit() {
         let password = document.getElementById("password").value;
   
         // Address of my backend
-        let request = `http://127.0.0.1:5000/load`;
+        let request = `http://127.0.0.1:5000/login`;
   
         // Send an HTTP GET request to the backend
-        let login_info = {"username" : username, "password" : password};
-        const response = await axios.post(request, login_info);
+        // let login_info = {"username" : username, "password" : password};
+        const response = await axios.get(request);
         console.log("data.data: ", JSON.stringify(response.data, null, 2));
     } catch (error) {
       console.log("error: ", error);
