243fbf6b54f983b46b6a7ff03896bd3351e245bd
Mingang Xu <xmgbg18@gmail.com>
2020-04-24 03:33:07 -0700

    frontend update

---
+++ frontend/public/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * An async function to send the request to the backend.
+ */
+async function submit() {
+  
+    // Set the mouse cursor to hourglass
+    document.body.style.cursor = "wait";
+    try {
+        // Get the login infos from the user
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+  
+        // Address of my backend
+        let request = `http://127.0.0.1:5000/load`;
+  
+        // Send an HTTP GET request to the backend
+        let login_info = {"username" : username, "password" : password};
+        const response = await axios.post(request, login_info);
+        console.log("data.data: ", JSON.stringify(response.data, null, 2));
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  
+    // Set the cursor back to default
+    document.body.style.cursor = "default";
+}
+  
