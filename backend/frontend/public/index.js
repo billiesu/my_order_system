457905f8bda32b6eefb234b47c1cc7795ad69674
Mingang Xu <xmgbg18@gmail.com>
2020-05-03 14:26:13 -0700

    final v2

--- backend/frontend/public/index.js
+++ backend/frontend/public/index.js
@@ -1,34 +1,8 @@
 'use strict';
 
-/**
- * An async function to send the request to the backend.
- */
-// async function submit() {
-  
-//     // Set the mouse cursor to hourglass
-//     document.body.style.cursor = "wait";
-//     try {
-//         // Get the login infos from the user
-//         let username = document.getElementById("username").value;
-//         let password = document.getElementById("password").value;
-  
-//         // Address of my backend
-//         let request = `http://www.tntrpg.com:5000/login`;
-  
-//         // Send an HTTP GET request to the backend
-//         // let login_info = {"username" : username, "password" : password};
-//         const response = await axios.get(request);
-//         console.log("data.data: ", JSON.stringify(response.data, null, 2));
-//     } catch (error) {
-//       console.log("error: ", error);
-//     }
-  
-//     // Set the cursor back to default
-//     document.body.style.cursor = "default";
-// }
 
+//when click the button, call email() function
 document.getElementById("emailSend").onclick = function () {
-  console.log("entry!!!");
   email();
 };
 
